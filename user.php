a2e1a722751be413ca1ea59fefddf8abc9a673ae
bopos2 <pakawat.work88@gmail.com>
2021-05-18 23:25:43 +0700

    template done

---
+++ user.php
@@ -0,0 +1,44 @@
+<?php include "./includes/header.php" ?>
+<?php include "./includes/navbar.php" ?>
+
+<!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+            <!-- Begin Page Content -->
+                <div class="container-fluid"> 
+                <!-- Page Heading -->
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-0 text-gray-800">รายชื่อ Admin</h1>
+                     </div>
+                     <table class="table">
+                        <thead>
+                          <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                          </tr>
+                          <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                          </tr>
+                          <tr>
+                            <th scope="row">3</th>
+                            <td colspan="2">Larry the Bird</td>
+                            <td>@twitter</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                </div>
+
+<?php include "./includes/scritp.php" ?>
+<?php include "./includes/footer.php" ?>
